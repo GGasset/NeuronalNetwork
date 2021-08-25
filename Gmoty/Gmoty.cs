fa6c563a611d545c8dcf8a083e695b2666d89a56
Germán Gasset <gassetgerman@gmail.com>
2021-08-25 14:11:17 +0200

    base commit

---
+++ Gmoty/Gmoty.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using NeuronalNetwork;
+using System.Collections.Generic;
+
+namespace Gmoty
+{
+    public class Gmoty
+    {
+
+
+
+        public Gmoty()
+        {
+
+        }
+    }
+
+    public class NetworkHolder
+    {
+        internal List<TemporalNetwork> networks;
+        internal List<NetworkConnections> connections;
+
+        public NetworkHolder(NetworkHolder networks)
+        {
+            this.networks = networks.networks;
+            connections = networks.connections;
+        }
+
+        /*public override string ToString()
+        {
+
+        }
+
+        public static NetworkHolder FromString(string value)
+        {
+
+        }*/
+    }
+
+    public class NetworkConnections
+    {
+        internal int fromNetworkIndex, toNetworkIndex;
+        internal Range fromOutputRange, toInputRange;
+    }
+
+    public class Range
+    {
+        public int start, end;
+
+        public Range(int start, int end)
+        {
+            this.start = start;
+            this.end = end;
+        }
+    }
+}
